817b342902bddc915ced874a88cec446a0d0d06c
omargnagy91 <omargnagy91@gmail.com>
2025-04-28 14:19:30 +0000

    fix

--- app/schemas.py
+++ app/schemas.py
@@ -4,4 +4,4 @@ class SavePayload(BaseModel):
     text: str
     part: int
     chapter: int
-    embedding: conlist(float, min_items=1536, max_items=1536)
+    embedding: conlist(float, min_length=1536, max_length=1536)
